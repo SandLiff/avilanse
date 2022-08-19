c77479e5419aec7f4f8c6cf78bd58164de4d9145
SandLiff <101722746+SandLiff@users.noreply.github.com>
2022-08-19 12:03:22 +0300

    Add files via upload

--- js/scipt.js
+++ js/scipt.js
@@ -1,6 +1,34 @@
 "use strict"
 
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
+
 const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
 if (iconMenu) {
 	const menuBody = document.querySelector('.menu__body');
 	iconMenu.addEventListener("click", function (e) {
